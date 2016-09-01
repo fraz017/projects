7573cd8daa27cd0958a85cbd9b9d999611649ac6
Fraz Ahsan <fraz.ahsan17@gmail.com>
2016-09-01 16:28:08 +0500

    Validation added

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Project do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :technologies, :is_active, :slug
+permit_params :name, :technologies, :is_active
 #
 # or
 #
